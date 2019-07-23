dc15db09397bdf018290b90183ae84d8c03b80e4
rprobst <rprobst@ivi.ru>
2019-07-23 12:51:17 +0300

    add router logo

--- application/src/pages/Home/Home.jsx
+++ application/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../logo.svg';
+import router_logo from '../../react-router.svg';
 import './Home.css'
 
 import Canvas from '../../components/Canvas/Canvas';
@@ -9,6 +10,7 @@ function Home() {
     return (
         <div className="Home">
             <Link className="Home__Link" to="/chat">Chat</Link>
+            <img src={router_logo} className="RouterLogo" alt="router logo" />
             <header className="Home__Header">
                 <img src={logo} className="Logo" alt="logo" />
             </header>
