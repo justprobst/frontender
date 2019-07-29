020327e43b17a2d071b47854a3a1937a60b21890
rprobst <rprobst@ivi.ru>
2019-07-29 10:26:22 +0300

    emit chat message

--- chat_server/index.js
+++ chat_server/index.js
@@ -13,6 +13,8 @@ io.on('connection', socket => {
 
     socket.on('chat message', message => {
         console.log('message: ' + message);
+
+        io.emit('chat message', message);
     });
 
     socket.on('disconnect', () => {
