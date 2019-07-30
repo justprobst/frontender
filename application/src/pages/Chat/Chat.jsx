5b6759c37f951981fc72ae22275f4d8cbd19915d
rprobst <rprobst@ivi.ru>
2019-07-30 16:35:44 +0300

    add username input

--- application/src/pages/Chat/Chat.jsx
+++ application/src/pages/Chat/Chat.jsx
@@ -7,26 +7,56 @@ import AddMessage from '../../containers/chat/AddMessage';
 
 import './Chat.css';
 
-function Chat() {
-    const socket = openSocket('http://localhost:8989');
+class Chat extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            userNameAccepted: false
+        }
+    }
 
-    socket.on('chat message', (message, user) => console.log(message + ' from ' + user));
+    setupSocket() {
+        this.socket = openSocket('http://localhost:8989');
+        this.socket.on('chat message', (message, user) => console.log(message + ' from ' + user));
+    }
 
-    const name = 'User' + Math.floor(Math.random() * 100);
-
-    return (
-        <div className="Chat">
-            <Link className="Chat__Link" to="/">Home</Link>
-            <h1 className="Chat__Title">WANNA CHAT ?</h1>
-            <div className="Chat__Window">
-                <Sidebar />
-                <div className="MessagesWindow">
-                    <MessagesList />
-                    <AddMessage sendMessageToSocket={message => socket.emit('chat message', message, name)}/>
-                </div>
+    render() {
+        return (
+            <div className="Chat">
+                <Link className="Chat__Link" to="/">Home</Link>
+                <h1 className="Chat__Title">{this.state.userNameAccepted ? "CHAT" : "WHAT IS YOUR NAME ?"}</h1>
+                {
+                    !this.state.userNameAccepted ?
+                    (
+                        <input
+                            className="NameInput"
+                            type="text"
+                            placeholder="Your name"
+                            value={this.state.username}
+                            onChange={(e) => this.setState({username: e.target.value})}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter' && this.state.username.length) {
+                                    this.setState({userNameAccepted: true});
+                                    this.setupSocket();
+                                }
+                            }}
+                        />
+                    )
+                        :
+                    (
+                        <div className="Chat__Window">
+                            <Sidebar />
+                            <div className="MessagesWindow">
+                                <MessagesList />
+                                <AddMessage sendMessageToSocket={message => this.socket.emit('chat message', message, this.state.username)}/>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Chat;
