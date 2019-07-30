d9e13c62279e69b2b78bc8c40d42b032bfd9b584
rprobst <rprobst@ivi.ru>
2019-07-30 11:29:20 +0300

    add id for actions

--- application/src/store/actions/messages.js
+++ application/src/store/actions/messages.js
@@ -1,13 +1,17 @@
 import { ADD_MESSAGE, MESSAGE_RECEIVED } from './ActionTypes';
 
+let nextMessageId = 0;
+
 export const AddMessage = (message, author) => ({
     type: ADD_MESSAGE,
+    id: nextMessageId++,
     message,
     author
 });
 
 export const messageReceived = (message, author) => ({
     type: MESSAGE_RECEIVED,
+    id: nextMessageId++,
     message,
     author
 });
--- application/src/store/actions/users.js
+++ application/src/store/actions/users.js
@@ -1,6 +1,9 @@
 import { ADD_USER } from './ActionTypes';
 
+let nextUserId = 0;
+
 export const addUser = name => ({
     type: ADD_USER,
+    id: nextUserId++,
     name
 });
