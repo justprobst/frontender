4e2c890705a54212b51c73c5e3644d87b136eaab
rprobst <rprobst@ivi.ru>
2019-07-30 11:50:34 +0300

    turn addMessage into class component

--- application/src/components/chat/AddMessage/AddMessage.jsx
+++ application/src/components/chat/AddMessage/AddMessage.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import './AddMessage.css';
 
-function AddMessage() {
-    return (
-        <div className="AddMessage">
-            <input className="AddMessage__Input" type="text" />
-            <button className="AddMessage__Button">Send</button>
-        </div>
-    );
+class AddMessage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ''
+        }
+    }
+
+    render() {
+        return (
+            <div className="AddMessage">
+                <input
+                    className="AddMessage__Input"
+                    type="text"
+                    placeholder="Message"
+                    value={this.state.input}
+                    onChange={
+                        (e) => this.setState({input: e.target.value})
+                    }
+                />
+                <button
+                    className="AddMessage__Button"
+                    onClick={
+                        () => this.props.onSubmit(this.state.input, 'Me')
+                    }
+                >Send</button>
+            </div>
+        );
+    }
 }
 
 export default AddMessage;
