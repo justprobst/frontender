47d5d04c57a8813570425800dcefb8a5e5d1ab25
rprobst <rprobst@ivi.ru>
2019-07-31 11:41:09 +0300

    add scroll handler and some style changes

--- application/src/components/chat/MessagesList/MessagesList.jsx
+++ application/src/components/chat/MessagesList/MessagesList.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import Message from '../Message/Message'
 import './MessagesList.css';
 
-function MessagesList(props) {
-    return (
-        <div className="MessagesList">
-            {
-                props.messages && props.messages.map((message, index) => (
-                    <Message key={index} {...message} />
-                ))
-            }
-        </div>
-    );
+class MessagesList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidUpdate() {
+        const messagesList = this.refs.messagesList;
+        messagesList.scrollTop = messagesList.scrollHeight;
+    }
+
+    render() {
+        return (
+            <div ref="messagesList" className="MessagesList">
+                {
+                    this.props.messages && this.props.messages.map((message, index) => (
+                        <Message key={index} {...message} />
+                    ))
+                }
+            </div>
+        );
+    }
 }
 
 export default MessagesList;
