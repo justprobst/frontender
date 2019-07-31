fb2a170f68a04367243ac44a9ee773ea0bd7c4b3
rprobst <rprobst@ivi.ru>
2019-07-31 16:14:16 +0300

    add technology logos

--- application/src/pages/Home/Home.jsx
+++ application/src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../logo.svg';
+import react_logo from '../../logo.svg';
 import router_logo from '../../react-router.svg';
+import redux_logo from '../../redux.svg';
+import socket_logo from '../../socket-io.svg';
+import nodejs_logo from '../../nodejs.svg';
+import express_logo from '../../express.png';
 import './Home.css'
 
 import Canvas from '../../components/home/Canvas/Canvas';
@@ -10,10 +14,12 @@ function Home() {
     return (
         <div className="Home">
             <Link className="Home__Link" to="/chat">Chat</Link>
-            <img src={router_logo} className="RouterLogo" alt="router logo" />
-            <header className="Home__Header">
-                <img src={logo} className="Logo" alt="logo" />
-            </header>
+            <img src={react_logo} className="Logo Logo_spin ReactLogo" alt="react logo" />
+            <img src={router_logo} className="Logo RouterLogo" alt="router logo" />
+            <img src={redux_logo} className="Logo ReduxLogo" alt="redux logo" />
+            <img src={socket_logo} className="SocketLogo" alt="socket logo" />
+            <img src={nodejs_logo} className="Logo NodeJsLogo" alt="nodejs logo" />
+            <img src={express_logo} className="Logo ExpressLogo" alt="express logo" />
             <Canvas/>
         </div>
     );
