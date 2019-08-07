2547a481a67f87696fe37fbc1c7c671046960677
rprobst <rprobst@ivi.ru>
2019-08-07 16:11:43 +0300

    add clocks functionality to socket logo

--- application/src/pages/Home/Home.jsx
+++ application/src/pages/Home/Home.jsx
@@ -11,13 +11,21 @@ import './Home.css'
 import Canvas from '../../components/home/Canvas/Canvas';
 
 function Home() {
+    const time = new Date().getHours();
+
     return (
         <div className="Home">
             <Link className="Home__Link" to="/chat">Chat</Link>
             <img src={react_logo} className="Logo Logo_spin ReactLogo" alt="react logo" />
             <img src={router_logo} className="Logo RouterLogo" alt="router logo" />
             <img src={redux_logo} className="Logo ReduxLogo" alt="redux logo" />
-            <img src={socket_logo} className="SocketLogo" alt="socket logo" />
+            <div className="SocketClock">
+                <span className="SocketClock__24">24</span>
+                <span className="SocketClock__15">15</span>
+                <span className="SocketClock__18">18</span>
+                <span className="SocketClock__21">21</span>
+                <img src={socket_logo} className="SocketLogo" alt="socket logo" style={{transform: `translateX(-50%) rotate(${time * 360 / 12 - 33}deg)`}} />
+            </div>
             <img src={nodejs_logo} className="Logo NodeJsLogo" alt="nodejs logo" />
             <img src={express_logo} className="Logo ExpressLogo" alt="express logo" />
             <Canvas/>
