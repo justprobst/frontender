f89a0d16df0a2701302fd271e72e18aba1195abe
rprobst <rprobst@ivi.ru>
2019-09-30 18:50:34 +0300

    split code into methods and functional components

--- application/src/pages/Chat/Chat.jsx
+++ application/src/pages/Chat/Chat.jsx
@@ -10,57 +10,74 @@ class Chat extends React.Component {
         super(props);
         this.state = {
             username: '',
-            userNameAccepted: false
+            userNameConfirmed: false
         }
     }
+
+    confirmName = () => {
+        if (this.state.username.length) {
+            this.setState({userNameConfirmed: true});
+            window.socket.emit('username', this.state.username);
+        }
+    };
+
+    onChange = (e) => {
+        this.setState({username: e.target.value})
+    };
 
     render() {
         return (
             <div className="Chat">
                 <Link className="Chat__Link" to="/">Home</Link>
-                <h1 className="Chat__Title">{this.state.userNameAccepted ? "CHAT" : "WHAT IS YOUR NAME ?"}</h1>
+                <h1 className="Chat__Title">
+                    {this.state.userNameConfirmed ? "CHAT" : "WHAT IS YOUR NAME ?"}
+                </h1>
                 {
-                    !this.state.userNameAccepted ?
-                    (
-                        <>
-                            <input
-                                className="NameInput"
-                                type="text"
-                                placeholder="Your name"
-                                value={this.state.username}
-                                onChange={(e) => this.setState({username: e.target.value})}
-                                onKeyDown={(e) => {
-                                    if (e.key === 'Enter' && this.state.username.length) {
-                                        this.setState({userNameAccepted: true});
-                                        window.socket.emit('username', this.state.username);
-                                    }
-                                }}
-                            />
-                            <button
-                                className="ConfirmNameButton"
-                                onClick={() => {
-                                    if (this.state.username.length) {
-                                        this.setState({userNameAccepted: true});
-                                        window.socket.emit('username', this.state.username);
-                                    }
-                                }}
-                            >CONFIRM NAME</button>
-                        </>
-                    )
-                        :
-                    (
-                        <div className="Chat__Window">
-                            <Sidebar />
-                            <div className="MessagesWindow">
-                                <MessagesList />
-                                <AddMessage sendMessageToSocket={message => window.socket.emit('chat message', message)}/>
-                            </div>
-                        </div>
-                    )
+                    this.state.userNameConfirmed ?
+                        <ChatWindow /> :
+                        <NameInput
+                            username={this.state.username}
+                            onChange={this.onChange}
+                            confirmName={this.confirmName}
+                        />
                 }
             </div>
         );
     }
+}
+
+function ChatWindow() {
+    return (
+        <div className="Chat__Window">
+            <Sidebar />
+            <div className="MessagesWindow">
+                <MessagesList />
+                <AddMessage sendMessageToSocket={
+                    message => window.socket.emit('chat message', message)
+                }/>
+            </div>
+        </div>
+    );
+}
+
+function NameInput(props) {
+    return (
+        <>
+            <input
+                className="NameInput"
+                type="text"
+                placeholder="Your name"
+                value={props.username}
+                onChange={props.onChange}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        props.confirmName();
+                    }
+                }}
+            />
+            <button className="ConfirmNameButton" onClick={props.confirmName}>CONFIRM NAME</button>
+        </>
+    )
 }
 
 export default Chat;
