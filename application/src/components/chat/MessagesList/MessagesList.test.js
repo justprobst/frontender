3a334169473c4ad32f7e3f5512cf5db788d3a4fe
rprobst <rprobst@ivi.ru>
2019-10-04 11:41:28 +0300

    messages list test

---
+++ application/src/components/chat/MessagesList/MessagesList.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import MessagesList from './MessagesList';
+
+describe('messages list', () => {
+    it('renders messages', () => {
+        const props = {
+            messages: [{}, {}, {}]
+        };
+
+        const messagesList = shallow(<MessagesList {...props} />);
+
+        expect(messagesList.children().length).toEqual(props.messages.length);
+    });
+});
